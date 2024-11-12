e91f18e54ac41dd9294b646758bfed4c7d042911
John Nelson <sigoofballde@gmail.com>
2024-11-12 15:17:13 -0600

    Fixed fetch-mock

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,11 +1,16 @@
 import { act, render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import Home from '@/app/page'
+import fetchMock from 'fetch-mock-jest'
+import { formattedAPIResponse } from 'testData/gemini'
 
 const setup = async () => {
   act(() => {
     render(<Home />)
   })
 }
+
+const apiUrl = `http://localhost:7069/getConversation`
 
 describe('Page display/manipulation', () => {
   test('Page displays correct fields', async () => {
@@ -15,8 +20,19 @@ describe('Page display/manipulation', () => {
   })
 })
 
-describe('Data display/manipulation', () => {
-  test('Quotes display when there are 3 quotes', () => {})
+describe.only('Data display/manipulation', () => {
+  test.only('Quotes display when there are 3 quotes', async () => {
+    fetchMock.getOnce(apiUrl, formattedAPIResponse)
+    setup()
+    userEvent.type(await screen.findByPlaceholderText('Conversation for reply'), 'Hello')
+    userEvent.click(await screen.findByRole('button', { name: 'Get Reply' }))
+
+    expect(
+      await screen.findAllByText(
+        `${formattedAPIResponse[0].quote} - ${formattedAPIResponse[0].reference}`,
+      ),
+    ).toBeInTheDocument()
+  })
   test('Quotes display when quote is missing', () => {})
   test('Quote displays when there is only 1', () => {})
 })
